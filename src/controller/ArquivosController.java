57243466937bc4cfc1f0cadc71c8bbdcdcb926b4
João Pedro <jplhfernandez@gmail.com>
2025-05-15 11:32:33 -0300

    Erros que não sinalizados

---
+++ src/controller/ArquivosController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ArquivosController implements IArquivosController {
+	
+	public ArquivosController() {
+		super();
+	}
+
+	@Override
+	public void readDir(String path) throws IOException {
+		// TODO Auto-generated method stub
+		File dir = new File(path);
+		if (dir.exists() && dir.isDirectory()) {
+			System.out.println("Arquivo Encontrado");
+		} else {
+			throw new IOException("Diretório Inválido");
+		}	
+	}
+
+	@Override
+	public void readFile(String path, String nome) throws IOException {
+		// TODO Auto-generated method stub
+		File arq = new File(path, nome);
+		if (arq.exists() && arq.isFile()) {
+			try {
+                InputStreamReader fluxo = new InputStreamReader(new FileInputStream(arq));
+				BufferedReader leitor = new BufferedReader(fluxo);
+				String linha = leitor.readLine();
+				while(linha != null) {
+					System.out.println(linha);
+				}
+				fluxo.close();
+				leitor.close();	
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		} else {
+			throw new IOException("Arquivo Inválido");
+		}
+	}	
+}
---
+++ src/view/Principal.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.io.IOException;
+import controller.ArquivosController;
+import controller.IArquivosController;
+
+public class Principal {
+
+	public static void main(String[] args) throws IOException {
+	    {
+	        IArquivosController arq = new ArquivosController();
+	        String dir = "C:\\Users\\JP\\AppData\\Local\\Temp";
+	        String nome = "generic_foods.csv";
+	        
+	        arq.readDir(dir);
+	        arq.readFile(dir, nome);
+	    }
+	}
+
+}
